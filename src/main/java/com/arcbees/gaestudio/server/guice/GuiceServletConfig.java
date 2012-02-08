ebadf16e781feb2278118eb5a024886f29550ade
Javier Fernandez-Ivern <javier@arcbees.com>
2012-02-08 14:43:07 -0500

    Simplified recorder module activation

--- src/main/java/com/arcbees/gaestudio/server/guice/GuiceServletConfig.java
+++ src/main/java/com/arcbees/gaestudio/server/guice/GuiceServletConfig.java
@@ -1,9 +1,6 @@
 package com.arcbees.gaestudio.server.guice;
 
-import com.arcbees.gaestudio.server.recorder.ApiProxyHook;
-import com.arcbees.gaestudio.server.recorder.DbOperationRecorderHookFactory;
 import com.arcbees.gaestudio.server.recorder.DbOperationRecorderModule;
-import com.google.apphosting.api.ApiProxy;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.servlet.GuiceServletContextListener;
@@ -11,17 +8,9 @@ import com.google.inject.servlet.GuiceServletContextListener;
 public class GuiceServletConfig extends GuiceServletContextListener {
 
     @Override
-    @SuppressWarnings("unchecked")
     protected Injector getInjector() {
-        Injector injector = Guice.createInjector(new ServerModule(), new DispatchServletModule(),
+        return Guice.createInjector(new ServerModule(), new DispatchServletModule(),
                 new DbOperationRecorderModule());
-
-        ApiProxyHook hook = new ApiProxyHook(ApiProxy.getDelegate());
-        hook.getHooks().put("datastore_v3",
-                injector.getProvider(DbOperationRecorderHookFactory.class).get().create(hook.getBaseDelegate()));
-        ApiProxy.setDelegate(hook);
-
-        return injector;
     }
 
 }
--- src/main/java/com/arcbees/gaestudio/server/recorder/DbOperationRecorderModule.java
+++ src/main/java/com/arcbees/gaestudio/server/recorder/DbOperationRecorderModule.java
@@ -22,6 +22,7 @@ import com.arcbees.gaestudio.shared.util.SimpleStackInspector;
 import com.arcbees.gaestudio.shared.util.StackInspector;
 import com.google.appengine.api.memcache.MemcacheService;
 import com.google.appengine.api.memcache.MemcacheServiceFactory;
+import com.google.apphosting.api.ApiProxy;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
@@ -30,6 +31,7 @@ import com.google.inject.servlet.RequestScoped;
 // TODO externalize magic strings
 public class DbOperationRecorderModule extends AbstractModule {
 
+    @SuppressWarnings("unchecked")
     @Override
     protected void configure() {
         bind(StackInspector.class).to(SimpleStackInspector.class);
@@ -39,6 +41,11 @@ public class DbOperationRecorderModule extends AbstractModule {
         install(new FactoryModuleBuilder()
                 .implement(DbOperationRecorderHook.class, DbOperationRecorderHook.class)
                 .build(DbOperationRecorderHookFactory.class));
+
+        ApiProxyHook hook = new ApiProxyHook(ApiProxy.getDelegate());
+        hook.getHooks().put("datastore_v3",
+                getProvider(DbOperationRecorderHookFactory.class).get().create(hook.getBaseDelegate()));
+        ApiProxy.setDelegate(hook);
     }
 
     @Provides
