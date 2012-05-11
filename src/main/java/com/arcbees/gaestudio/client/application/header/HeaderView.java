8b6c76839c673f59cc087960698c5a7b91ff7d3d
Vincent Roseberry <roseberryv@gmail.com>
2012-05-11 16:00:21 -0400

    Automatically stop listening when user close the tab/window

--- src/main/java/com/arcbees/gaestudio/client/application/header/HeaderView.java
+++ src/main/java/com/arcbees/gaestudio/client/application/header/HeaderView.java
@@ -4,9 +4,12 @@ import com.arcbees.core.client.mvp.ViewWithUiHandlers;
 import com.arcbees.core.client.mvp.uihandlers.UiHandlersStrategy;
 import com.arcbees.gaestudio.client.Resources;
 import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.logical.shared.CloseEvent;
+import com.google.gwt.event.logical.shared.CloseHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
@@ -32,6 +35,13 @@ public class HeaderView extends ViewWithUiHandlers<HeaderUiHandlers> implements 
         this.resources = resources;
         initWidget(uiBinder.createAndBindUi(this));
         setRecordingState();
+
+        Window.addCloseHandler(new CloseHandler<Window>() {
+            @Override
+            public void onClose(CloseEvent<Window> windowCloseEvent) {
+                getUiHandlers().onToggleRecording(false);
+            }
+        });
     }
 
     @UiHandler("recording")
