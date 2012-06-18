23a5bbeba785bbb44a7e5ac3aee82c36294154a5
Vincent Roseberry <roseberryv@gmail.com>
2012-06-18 16:44:12 -0400

    Supress warnings in PropertiesDeserializer

--- src/main/java/com/google/appengine/api/datastore/PropertiesDeserializer.java
+++ src/main/java/com/google/appengine/api/datastore/PropertiesDeserializer.java
@@ -14,6 +14,7 @@ import java.util.Map;
 public class PropertiesDeserializer implements JsonDeserializer<Map> {
 
     @Override
+    @SuppressWarnings("unchecked")
     public Map deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws
             JsonParseException {
         Map<String, Object> properties = new HashMap<String, Object>();
