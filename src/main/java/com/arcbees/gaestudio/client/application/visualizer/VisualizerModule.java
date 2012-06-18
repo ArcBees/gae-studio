694b91f5083b977d23a41f94a30113846236dabd
Vincent Roseberry <roseberryv@gmail.com>
2012-06-18 16:49:05 -0400

    Visualizer Module cleanup

--- src/main/java/com/arcbees/gaestudio/client/application/visualizer/VisualizerModule.java
+++ src/main/java/com/arcbees/gaestudio/client/application/visualizer/VisualizerModule.java
@@ -8,7 +8,12 @@ import com.arcbees.gaestudio.client.application.visualizer.widget.EntityDetailsV
 import com.arcbees.gaestudio.client.application.visualizer.widget.EntityListPresenter;
 import com.arcbees.gaestudio.client.application.visualizer.widget.EntityListUiHandlers;
 import com.arcbees.gaestudio.client.application.visualizer.widget.EntityListView;
-import com.arcbees.gaestudio.client.application.visualizer.widget.WidgetModule;
+import com.arcbees.gaestudio.client.application.visualizer.widget.KindListPresenter;
+import com.arcbees.gaestudio.client.application.visualizer.widget.KindListUiHandlers;
+import com.arcbees.gaestudio.client.application.visualizer.widget.KindListView;
+import com.arcbees.gaestudio.client.application.visualizer.widget.VisualizerToolbarPresenter;
+import com.arcbees.gaestudio.client.application.visualizer.widget.VisualizerToolbarUiHandlers;
+import com.arcbees.gaestudio.client.application.visualizer.widget.VisualizerToolbarView;
 import com.google.inject.TypeLiteral;
 import com.gwtplatform.mvp.client.gin.AbstractPresenterModule;
 
@@ -16,23 +21,32 @@ public class VisualizerModule extends AbstractPresenterModule {
 
     @Override
     protected void configure() {
-        install(new WidgetModule());
-
-        bind(new TypeLiteral<UiHandlersStrategy<EntityListUiHandlers>>() {})
-                .to(new TypeLiteral<ProviderUiHandlersStrategy<EntityListUiHandlers>>() {});
-        bind(new TypeLiteral<UiHandlersStrategy<EntityDetailsUiHandlers>>() {})
-                .to(new TypeLiteral<ProviderUiHandlersStrategy<EntityDetailsUiHandlers>>() {});
+        bindPresenter(VisualizerPresenter.class, VisualizerPresenter.MyView.class,
+                VisualizerView.class, VisualizerPresenter.MyProxy.class);
 
         bindSingletonPresenterWidget(EntityListPresenter.class, EntityListPresenter.MyView.class,
                 EntityListView.class);
+        bind(EntityListUiHandlers.class).to(EntityListPresenter.class);
+        bind(new TypeLiteral<UiHandlersStrategy<EntityListUiHandlers>>() {})
+                .to(new TypeLiteral<ProviderUiHandlersStrategy<EntityListUiHandlers>>() {});
+
+        bindSingletonPresenterWidget(VisualizerToolbarPresenter.class, VisualizerToolbarPresenter.MyView.class,
+                VisualizerToolbarView.class);
+        bind(VisualizerToolbarUiHandlers.class).to(VisualizerToolbarPresenter.class);
+        bind(new TypeLiteral<UiHandlersStrategy<VisualizerToolbarUiHandlers>>() {}).to(
+                new TypeLiteral<ProviderUiHandlersStrategy<VisualizerToolbarUiHandlers>>() {});
+
+        bindSingletonPresenterWidget(KindListPresenter.class, KindListPresenter.MyView.class,
+                KindListView.class);
+        bind(KindListUiHandlers.class).to(KindListPresenter.class);
+        bind(new TypeLiteral<UiHandlersStrategy<KindListUiHandlers>>() {}).to(
+                new TypeLiteral<ProviderUiHandlersStrategy<KindListUiHandlers>>() {});
+
         bind(EntityDetailsPresenter.class).asEagerSingleton();
         bind(EntityDetailsPresenter.MyView.class).to(EntityDetailsView.class);
-
-        bind(EntityListUiHandlers.class).to(EntityListPresenter.class);
         bind(EntityDetailsUiHandlers.class).to(EntityDetailsPresenter.class);
-
-        bindPresenter(VisualizerPresenter.class, VisualizerPresenter.MyView.class,
-                VisualizerView.class, VisualizerPresenter.MyProxy.class);
+        bind(new TypeLiteral<UiHandlersStrategy<EntityDetailsUiHandlers>>() {})
+                .to(new TypeLiteral<ProviderUiHandlersStrategy<EntityDetailsUiHandlers>>() {});
     }
 
 }
