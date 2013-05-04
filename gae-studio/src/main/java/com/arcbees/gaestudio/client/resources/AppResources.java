c699154dd1ecb9a9908ebc8472a83408fefb164a
albre <alexandre.breton@arcbees.com>
2013-05-04 16:35:22 -0400

    Change logo and vizualizer/profiler buttons

--- gae-studio/src/main/java/com/arcbees/gaestudio/client/resources/AppResources.java
+++ gae-studio/src/main/java/com/arcbees/gaestudio/client/resources/AppResources.java
@@ -49,41 +49,33 @@ public interface AppResources extends ClientBundle {
         String topmenuBg();
 
         String topmenuBgHover();
+
+        String topmenuBgActive();
     }
 
     public Styles styles();
 
     public Sprites sprites();
 
-    @Source("images/topmenuBg.jpg")
     @ImageResource.ImageOptions(repeatStyle = ImageResource.RepeatStyle.Horizontal)
     ImageResource topmenuBg();
 
-    @Source("images/topmenuBgHover.png")
     @ImageResource.ImageOptions(repeatStyle = ImageResource.RepeatStyle.Horizontal)
     ImageResource topmenuBgHover();
 
-    @Source("images/logo.png")
     ImageResource logo();
 
-    @Source("images/topmenuSeparator.png")
     ImageResource topmenuSeparator();
 
-    @Source("toolbar/record.png")
     ImageResource record();
 
-    @Source("toolbar/stop.png")
     ImageResource stop();
 
-    @Source("toolbar/delete.png")
     ImageResource delete();
 
-    @Source("toolbar/refresh.png")
     ImageResource refresh();
 
-    @Source("toolbar/edit.png")
     ImageResource edit();
 
-    @Source("toolbar/create.png")
     ImageResource create();
 }
