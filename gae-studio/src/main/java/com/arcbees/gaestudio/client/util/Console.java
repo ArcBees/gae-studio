77ac3be79e88c3b728dea82b7d0609301c8c57ef
spgingras <simon.pierre.gingras@gmail.com>
2013-05-07 10:30:50 -0400

    CR Fix

--- gae-studio/src/main/java/com/arcbees/gaestudio/client/util/Console.java
+++ gae-studio/src/main/java/com/arcbees/gaestudio/client/util/Console.java
@@ -1,7 +1,7 @@
 package com.arcbees.gaestudio.client.util;
 
 public class Console {
-    public static native void log(String message)/*-{
+    public static native void log(String message) /*-{
         $wnd.console.log(message)
     }-*/;
 }
