84c7ebf9533debd76f0c20a3e8f238f99622732d
Maxime Meriouma-Caron <maxime.meriouma@arcbees.com>
2013-08-13 14:39:22 -0400

    Added interceptor for Google Analytics

--- gae-studio/gae-studio-server/src/main/java/com/arcbees/gaestudio/server/rest/RestModule.java
+++ gae-studio/gae-studio-server/src/main/java/com/arcbees/gaestudio/server/rest/RestModule.java
@@ -9,20 +9,10 @@
 
 package com.arcbees.gaestudio.server.rest;
 
-import javax.inject.Singleton;
-
-import com.arcbees.gaestudio.server.GoogleAnalyticConstants;
-import com.arcbees.gaestudio.server.util.GoogleAnalyticClientIdProvider;
-import com.arcbees.googleanalytic.GoogleAnalytic;
 import com.google.inject.AbstractModule;
-import com.google.inject.Provides;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
 
 public class RestModule extends AbstractModule {
-    private static final String TRACKING_CODE = "UA-41550930-4";
-    private static final String APPLICATION_NAME = "GAE-Studio";
-    private static final String APPLICATION_VERSION = "1.0";
-
     @Override
     protected void configure() {
         install(new FactoryModuleBuilder().build(SubresourceFactory.class));
@@ -32,17 +22,5 @@ public class RestModule extends AbstractModule {
         bind(EntitiesResource.class);
         bind(OperationsResource.class);
         bind(RecordResource.class);
-    }
-
-    @Provides
-    @Singleton
-    GoogleAnalytic createGoogleAnalytic(GoogleAnalyticClientIdProvider clientIdProvider) {
-        GoogleAnalytic googleAnalytic
-                = GoogleAnalytic.build(clientIdProvider.get(), TRACKING_CODE, APPLICATION_NAME, APPLICATION_VERSION);
-
-        googleAnalytic.trackEvent(GoogleAnalyticConstants.CAT_INITIALIZATION,
-                GoogleAnalyticConstants.APPLICATION_LOADED);
-
-        return googleAnalytic;
     }
 }
