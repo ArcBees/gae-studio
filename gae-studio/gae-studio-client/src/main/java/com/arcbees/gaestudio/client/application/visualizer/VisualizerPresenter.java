67b1503ceb42c7ee8214fc9fbfce9c9548510e22
jdramaix <julien.dramaix@gmail.com>
2013-08-26 16:18:03 +0200

    remove unnecessary space

--- gae-studio/gae-studio-client/src/main/java/com/arcbees/gaestudio/client/application/visualizer/VisualizerPresenter.java
+++ gae-studio/gae-studio-client/src/main/java/com/arcbees/gaestudio/client/application/visualizer/VisualizerPresenter.java
@@ -39,14 +39,13 @@ public class VisualizerPresenter extends Presenter<VisualizerPresenter.MyView,
     }
 
     @ContentSlot
-    public static final GwtEvent.Type<RevealContentHandler<?>> SLOT_ENTITIES  = new GwtEvent
+    public static final GwtEvent.Type<RevealContentHandler<?>> SLOT_ENTITIES = new GwtEvent
             .Type<RevealContentHandler<?>>();
     public static final Object SLOT_TOOLBAR = new Object();
     public static final Object SLOT_KINDS = new Object();
     @ContentSlot
     public static final GwtEvent.Type<RevealContentHandler<?>> SLOT_ENTITY_DETAILS = new GwtEvent
             .Type<RevealContentHandler<?>>();
-
     @ContentSlot
     public static final GwtEvent.Type<RevealContentHandler<?>> SLOT_ENTITY_DELETION = new GwtEvent
             .Type<RevealContentHandler<?>>();
