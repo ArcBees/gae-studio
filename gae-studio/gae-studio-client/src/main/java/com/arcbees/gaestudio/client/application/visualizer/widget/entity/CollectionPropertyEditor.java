8e347ff0734dcd0d75abb72a7b1125f6017231f0
spg <simon.pierre.gingras@arcbees.com>
2013-11-19 09:08:48 -0500

    Cr fixes

--- gae-studio/gae-studio-client/src/main/java/com/arcbees/gaestudio/client/application/visualizer/widget/entity/CollectionPropertyEditor.java
+++ gae-studio/gae-studio-client/src/main/java/com/arcbees/gaestudio/client/application/visualizer/widget/entity/CollectionPropertyEditor.java
@@ -1,3 +1,12 @@
+/**
+ * Copyright (c) 2013 by ArcBees Inc., All rights reserved.
+ * This source code, and resulting software, is the confidential and proprietary information
+ * ("Proprietary Information") and is the intellectual property ("Intellectual Property")
+ * of ArcBees Inc. ("The Company"). You shall not disclose such Proprietary Information and
+ * shall use it only in accordance with the terms and conditions of any and all license
+ * agreements you have entered into with The Company.
+ */
+
 package com.arcbees.gaestudio.client.application.visualizer.widget.entity;
 
 import java.util.Collection;
@@ -15,8 +24,8 @@ import com.google.inject.assistedinject.Assisted;
 import static com.arcbees.gaestudio.shared.PropertyName.VALUE;
 
 public class CollectionPropertyEditor extends AbstractPropertyEditor<Collection<?>> {
-
     PropertyEditorCollectionWidget widget;
+
     private JSONValue property;
 
     @Inject
