28766644df0a2c57924ea5254dc6471f8eba8b10
spg <simon.pierre.gingras@arcbees.com>
2013-11-22 12:47:40 -0500

    Dead code

--- gae-studio/gae-studio-client/src/main/java/com/arcbees/gaestudio/client/application/profiler/event/RecordingStateChangedEvent.java
+++ gae-studio/gae-studio-client/src/main/java/com/arcbees/gaestudio/client/application/profiler/event/RecordingStateChangedEvent.java
@@ -12,7 +12,6 @@ package com.arcbees.gaestudio.client.application.profiler.event;
 import com.google.gwt.event.shared.EventHandler;
 import com.google.gwt.event.shared.GwtEvent;
 import com.google.gwt.event.shared.HasHandlers;
-import com.google.web.bindery.event.shared.HandlerRegistration;
 
 public class RecordingStateChangedEvent extends GwtEvent<RecordingStateChangedEvent.RecordingStateChangedHandler> {
     private boolean starting;
@@ -30,14 +29,6 @@ public class RecordingStateChangedEvent extends GwtEvent<RecordingStateChangedEv
     public static void fire(HasHandlers source, boolean starting, Long currentRecordId) {
         RecordingStateChangedEvent eventInstance = new RecordingStateChangedEvent(starting, currentRecordId);
         source.fireEvent(eventInstance);
-    }
-
-    public static void fire(HasHandlers source, RecordingStateChangedEvent eventInstance) {
-        source.fireEvent(eventInstance);
-    }
-
-    public interface HasRecordingStateChangedHandlers extends HasHandlers {
-        HandlerRegistration addRecordingStateChangedHandler(RecordingStateChangedHandler handler);
     }
 
     public interface RecordingStateChangedHandler extends EventHandler {
