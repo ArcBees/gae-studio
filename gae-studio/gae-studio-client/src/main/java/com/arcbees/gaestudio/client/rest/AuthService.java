4ea23836b77c41c3138aa3f303eadd7b22cfe26e
maximemeriouma <maxime.meriouma@arcbees.com>
2014-02-12 15:44:03 -0500

    Format

--- gae-studio/gae-studio-client/src/main/java/com/arcbees/gaestudio/client/rest/AuthService.java
+++ gae-studio/gae-studio-client/src/main/java/com/arcbees/gaestudio/client/rest/AuthService.java
@@ -17,7 +17,6 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 
-import com.arcbees.gaestudio.shared.auth.Token;
 import com.arcbees.gaestudio.shared.auth.User;
 import com.arcbees.gaestudio.shared.rest.EndPoints;
 import com.arcbees.gaestudio.shared.rest.UrlParameters;
@@ -43,7 +42,7 @@ public interface AuthService extends RestService {
     @Path(EndPoints.LOGIN)
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     RestAction<User> login(@FormParam(UrlParameters.EMAIL) String email,
-                            @FormParam(UrlParameters.PASSWORD) String password);
+                           @FormParam(UrlParameters.PASSWORD) String password);
 
     @DELETE
     @Path(EndPoints.LOGIN)
