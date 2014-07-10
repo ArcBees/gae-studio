fea05f3d3c011792a3cb36c601a17ef621853ffa
Maxime Mériouma-Caron <maxime.meriouma@arcbees.com>
2014-07-10 16:28:00 -0400

    Fixed merge

--- gae-studio/gae-studio-client/src/main/java/com/arcbees/gaestudio/client/application/visualizer/widget/EntityListView.java
+++ gae-studio/gae-studio-client/src/main/java/com/arcbees/gaestudio/client/application/visualizer/widget/EntityListView.java
@@ -21,7 +21,6 @@ import com.arcbees.gaestudio.shared.dto.entity.EntityDto;
 import com.arcbees.gaestudio.shared.dto.entity.KeyDto;
 import com.google.gwt.dom.client.BrowserEvents;
 import com.google.gwt.dom.client.DivElement;
-import com.google.gwt.dom.client.Element;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.logical.shared.AttachEvent;
 import com.google.gwt.event.shared.HandlerRegistration;
