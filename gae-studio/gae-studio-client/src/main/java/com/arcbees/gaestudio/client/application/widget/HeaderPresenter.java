1147a2b62d7594e37b602ac2920f71ae7b94b5fa
Olivier Lafleur <olivier.lafleur@cll.qc.ca>
2014-07-11 17:51:14 -0400

    Remove logout, visualizer menu and theme change

--- gae-studio/gae-studio-client/src/main/java/com/arcbees/gaestudio/client/application/widget/HeaderPresenter.java
+++ gae-studio/gae-studio-client/src/main/java/com/arcbees/gaestudio/client/application/widget/HeaderPresenter.java
@@ -10,59 +10,27 @@
 package com.arcbees.gaestudio.client.application.widget;
 
 import com.arcbees.gaestudio.client.place.NameTokens;
-import com.arcbees.gaestudio.client.rest.AuthService;
-import com.arcbees.gaestudio.client.util.AsyncCallbackImpl;
-import com.arcbees.gaestudio.client.util.CurrentUser;
 import com.google.inject.Inject;
 import com.google.web.bindery.event.shared.EventBus;
-import com.gwtplatform.dispatch.rest.shared.RestAction;
-import com.gwtplatform.dispatch.rest.shared.RestDispatch;
-import com.gwtplatform.mvp.client.HasUiHandlers;
 import com.gwtplatform.mvp.client.PresenterWidget;
 import com.gwtplatform.mvp.client.View;
 import com.gwtplatform.mvp.client.proxy.PlaceManager;
 import com.gwtplatform.mvp.shared.proxy.PlaceRequest;
 
-public class HeaderPresenter extends PresenterWidget<HeaderPresenter.MyView> implements HeaderUiHandlers {
-    interface MyView extends View, HasUiHandlers<HeaderUiHandlers> {
+public class HeaderPresenter extends PresenterWidget<HeaderPresenter.MyView> {
+    interface MyView extends View {
         void setProfilerActive();
     }
 
     private final PlaceManager placeManager;
-    private final AuthService authService;
-    private final RestDispatch dispatch;
-    private final CurrentUser currentUser;
 
     @Inject
     HeaderPresenter(EventBus eventBus,
                     MyView view,
-                    PlaceManager placeManager,
-                    AuthService authService,
-                    RestDispatch dispatch,
-                    CurrentUser currentUser) {
+                    PlaceManager placeManager) {
         super(eventBus, view);
 
         this.placeManager = placeManager;
-        this.authService = authService;
-        this.dispatch = dispatch;
-        this.currentUser = currentUser;
-
-        getView().setUiHandlers(this);
-    }
-
-    @Override
-    public void logout() {
-        RestAction<Void> action = authService.logout();
-
-        dispatch.execute(action, new AsyncCallbackImpl<Void>() {
-            @Override
-            public void onSuccess(Void result) {
-                currentUser.setLoggedIn(false);
-
-                PlaceRequest placeRequest = new PlaceRequest.Builder().nameToken(NameTokens.auth).build();
-                placeManager.revealPlace(placeRequest);
-            }
-        });
     }
 
     @Override
