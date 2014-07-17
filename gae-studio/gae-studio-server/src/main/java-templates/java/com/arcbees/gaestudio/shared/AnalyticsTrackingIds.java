f5d7ba7617346adbbe7ee204dfe8fcd9b38171c2
spgingras <simon.pierre.gingras@arcbees.com>
2014-07-17 17:13:58 -0400

    Analytics Tracking Ids

---
+++ gae-studio/gae-studio-server/src/main/java-templates/java/com/arcbees/gaestudio/shared/AnalyticsTrackingIds.java
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) 2014 by ArcBees Inc., All rights reserved.
+ * This source code, and resulting software, is the confidential and proprietary information
+ * ("Proprietary Information") and is the intellectual property ("Intellectual Property")
+ * of ArcBees Inc. ("The Company"). You shall not disclose such Proprietary Information and
+ * shall use it only in accordance with the terms and conditions of any and all license
+ * agreements you have entered into with The Company.
+ */
+
+package com.arcbees.gaestudio.server;
+
+public class AnalyticsTrackingIds {
+    // will be replaced by maven
+    public static final String SERVER_TRACKING_ID  = "${gaes.analytics.trackingid.server}";
+    public static final String CLIENT_TRACKING_ID  = "${gaes.analytics.trackingid.client}";
+}
